16584772327c73b49216ebf1a37bc1098d66bbce
gujiaojiao <gjj19992021@163.com>
2023-07-27 21:33:05 +0800

    初次体验- 提交没有格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>我好想去看五月天的演唱会啊！！！
+        </h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+//main.ts是入口文件
+
+const fun = () => {
+  console.log("盛夏光年");
+}
+fun();
