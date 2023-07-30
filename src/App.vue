89a12ed6ec751d7056262fb0b394ba6436e440fc
gujiaojiao <gjj19992021@163.com>
2023-07-30 23:24:40 +0800

    第二次

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,10 @@
 </template>
 
 <script setup lang="ts">
-
+for(let i=0;i<100;i++){
+    console.log(i++);
+}
+let str="五月天的粉丝-Wmls";
 </script>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ createApp(App).mount('#app')
 //main.ts是入口文件
 
 const fun = () => {
-  console.log("盛夏光年");
+  console.log("盛夏光年我没抢到五月天的演唱会门票");
 }
 fun();
