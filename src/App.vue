351bfa1f53dc9d13fc46486ae209d5fb81ba5a1e
gujiaojiao <gjj19992021@163.com>
2023-07-31 19:56:17 +0800

    格式化体验

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-    <div>
-        <h1>我好想去看五月天的演唱会啊！！！
-        </h1>
-    </div>
+  <div>
+    <h1>我好想去看五月天的演唱会啊！！！</h1>
+  </div>
 </template>
 
 <script setup lang="ts">
-for(let i=0;i<100;i++){
-    console.log(i++);
+for (let i = 0; i < 100; i++) {
+  console.log(i++)
 }
-let str="五月天的粉丝-Wmls";
+let str = '五月天的粉丝-Wmls'
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
