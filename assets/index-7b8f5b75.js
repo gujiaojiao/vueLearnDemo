b598f8e80a7f3dadb8f6f904ef8cff6308b45602
gujiaojiao <gjj19992021@163.com>
2025-12-23 11:10:28 +0800

    部署 Vue 静态文件到 gh-pages

---
+++ assets/index-7b8f5b75.js
@@ -0,0 +1,1 @@
+import{_ as e,o,e as t,k as c}from"./index-1266dafb.js";const n={},s=c("h1",null,"我是一级路由404",-1),_=[s];function r(a,d){return o(),t("div",null,_)}const i=e(n,[["render",r]]);export{i as default};
